b4b736f3a4ad9683536b0913d473724178df3c35
WildWolf <magnus1997.mho@gmail.com>
2022-06-29 13:51:19 +0200

    fix(front-end): remove models from home page

--- ClientApp/src/pages/Home.tsx
+++ ClientApp/src/pages/Home.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import Models from "./Models";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
+    const navigate = useNavigate();
     return (
         <div>
             <h1 className="align-middle">
                 Welcome to ModelSaber <label style={{ display: "inline", fontSize: 1, textDecorationLine: "line-through", opacity: 0.1 }}><i>(Destroyer of old links)</i></label>
             </h1>
-            <Models />
+            <a onClick={() => navigate("/models")} className="btn btn-primary">Models</a>
         </div>
     );
 };
